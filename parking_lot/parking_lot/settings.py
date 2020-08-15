901a70202364b235597e4df9baf946a10890f76b
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-08-15 16:02:38 +0530

    [Sachin]Add.created connection with  mongoDB

--- parking_lot/parking_lot/settings.py
+++ parking_lot/parking_lot/settings.py
@@ -73,13 +73,21 @@ WSGI_APPLICATION = 'parking_lot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+# Mongo DB database connection
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'djongo',
+         'NAME': 'parking_lot',
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
