6dce14cd1724e19ee8c2794b6d73ec856ef2fa76
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-08-18 18:26:20 +0530

    [sachin]Add.created new model for parking system

--- parking_system/parking/models.py
+++ parking_system/parking/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class Parking(models.Model):
+    """Model definition for Parking."""
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for Parking."""
+
+        verbose_name = 'Parking'
+        verbose_name_plural = 'Parkings'
+
+    def __str__(self):
+        """Unicode representation of Parking."""
+        pass
