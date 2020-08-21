c019fc13de2e4f57724d302cb833e1da37e61cfc
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-08-21 16:41:07 +0530

    [sachin]Add.Add logout Exception

--- parking_system/user/UseException.py
+++ parking_system/user/UseException.py
@@ -3,3 +3,6 @@ from rest_framework.exceptions import APIException
 class RoleNotExist(APIException):
     detail = "Mentioned Role is not present Under User"
     code = 404
+
+class LogOutFailed(APIException):
+    detail = "Failed to SignOut,Please Try again later"
