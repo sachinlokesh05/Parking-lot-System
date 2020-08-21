02675cfc84f161184c3d741a78480300a2d4af06
sachinlokesh05 <sachin.beee.15@acharya.ac.in>
2020-08-21 16:33:14 +0530

    [sachin]Add.refactored register API

--- parking_system/user/views.py
+++ parking_system/user/views.py
@@ -5,24 +5,26 @@ from django.contrib.auth.models import Group, User
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from django.core.exceptions import ValidationError
+from django.core.exceptions import FieldDoesNotExist
+from rest_framework.exceptions import NotFound,AuthenticationFailed
 from .UseException import RoleNotExist
+from .forms import LoginForm
+from .my_custom_backend import EmailOrUsernameModelBackend
+from django.contrib.auth import login
+
 class Register(APIView):
-    queryset = User
     serializer_class = RegisterUserSerializer
 
     def post(self, request):
         serializer = RegisterUserSerializer(data=request.data)
 
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        print(request.data['roles'])
 
         try:
-            print(request.data['roles'])
             group = Group.objects.get(name=request.data['roles'])
-            print(group)
-            user.groups.add(group)
         except Exception :
-            raise RoleNotExist
+            return Response(data="Role Does not Exist",status=status.HTTP_404_NOT_FOUND)
+        user = serializer.save()    
+        user.groups.add(group)    
         return Response(data="Succefully Registered",status=status.HTTP_201_CREATED)
+
